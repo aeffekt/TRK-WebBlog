a522e00e79a3eecaff8d544df805f2afdd43e9f8
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-08-27 15:14:48 -0300

    allowed hots added

--- django_webblog/settings.py
+++ django_webblog/settings.py
@@ -15,9 +15,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['turkian.pythonanywhere.com']
 
 
 # Application definition
